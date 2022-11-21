2290e69c41c3dfdf2223f2d63170d482591560a2
Justin Meyer <111370382+jmeyer25@users.noreply.github.com>
2022-11-21 12:12:19 -0500

    Update MAIN_PROJECT.py
    Making some changes to the overall look of the Tkinter GUI.

--- MAIN_PROJECT.py
+++ MAIN_PROJECT.py
@@ -12,6 +12,7 @@ def convert_file():
 
     #assigning the file path for the tif image
     file_path = 'D:\Random Stuff\image.tif'
+    ######## Want to make the file_path a possible input for users to load custom DEM files through GUI instead of changing code #######
 
     #opening the tif file and assigning it
     file = gdal.Open(file_path)
@@ -40,25 +41,31 @@ def convert_file():
 x = tk.Tk()
 
 #window size
-x.geometry("375x400")
+x.geometry("400x350")
 
 #window title
-x.title("GUI Program")
+x.title("3D Terrain Generation")
+x.configure(bg = "#9A9AC0")
 
 
 
 #button properties, command is the function/action performed
-dem_button = tk.Button(x, text = "Create 3D Elevation Model", width = 25, height = 10, activebackground = "red", command = convert_file)
+dem_button = tk.Button(x, text = "Create 3D Elevation Model", width = 30, height = 10, bg = "grey", activebackground = "dark slate gray", bd = 6, command = convert_file)
 dem_button.grid(row = 1, column = 1)
 
-browse_button = tk.Button(x, text = "Browse Files...", width = 25, height = 10, activebackground = "red")
+browse_button = tk.Button(x, text = "Browse Files...", width = 20, height = 5, bg = "grey", activebackground = "dark slate gray", bd = 6)
 browse_button.grid(row = 1, column = 2)
 
-########################### want to insert another button to call a function that will generate a random DEM array #############################
+random_generation_button = tk.Button(x, text = "Generate Unique/Random 3D Terrain", width = 30, height = 10, bg = "grey", activebackground = "dark slate gray", bd = 6)
+random_generation_button.grid(row = 2, column = 1)
 
 
 #running the "x" GUI variables
 x.mainloop()
+
+
+
+
 
 
 ##################want to perhaps run code from another file???##################################
